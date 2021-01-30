fb6311e1a264900e5e333b72ac17f6777f020154
Manu Cheremeh <manucheremeh1995@gmail.com>
2021-01-30 23:30:14 +0000

    feat: Profile picture display on navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,8 @@ module ApplicationHelper
       p 
     end
   end
+
+  def current_user_profile_image(default_image)
+    current_user ? current_user.displayed_image(default_image) : default_image
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,15 @@
 # Table name: users
 #
 #  id                     :uuid             not null, primary key
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :inet
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  first_name             :string
 #  intro                  :text
-#  last_login             :date
 #  last_name              :string
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :inet
 #  middle_name            :string
 #  phone_number           :string(30)
 #  profile                :text
@@ -31,6 +34,11 @@
 #
 class User < ApplicationRecord
 
+  # constants
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # VALID_USERNAME_REGEX = /\A[a-zA-Z0-9\-\_]+\z/
+  USERNAME_EXCLUSIONS = %w[admin root user password]
+
   extend FriendlyId
   # attributes
   attr_reader :slug
@@ -39,19 +47,26 @@ class User < ApplicationRecord
   after_create :create_subscription
   # validations
   validates :slug, uniqueness: true
+  validates :email, format: { with: VALID_EMAIL_REGEX }, presence: true
+  # validates :username, format: { with: VALID_USERNAME_REGEX },
+  #           exclusion: { in: USERNAME_EXCLUSIONS, message: :duplicate },
+  #           uniqueness: { case_sensitive: false }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # relationships
   has_many :followed_users, foreign_key: :follower_id, class_name: 'Friendship'
   has_many :followees, through: :followed_users
-
   has_many :following_users, foreign_key: :followee_id, class_name: 'Friendship'
   has_many :followers, through: :following_users
   has_one :subscription
+  has_one :admin
+  has_one_attached :image
 
   # friendly ID
-  friendly_id :first_name, use: [:slugged, :finders]
+  friendly_id :first_name, use: %i[slugged finders]
 
   # methods
   def generate_jwt
@@ -60,5 +75,17 @@ class User < ApplicationRecord
 
   def create_subscription
     Subscription.create(user_id: id) if subscription.nil?
+  end
+
+  def full_name
+    [first_name, middle_name, last_name].compact.join(' ')
+  end
+
+  def displayed_image(default_image)
+    if image.attached?
+      image
+    else
+      default_image
+    end
   end
 end
